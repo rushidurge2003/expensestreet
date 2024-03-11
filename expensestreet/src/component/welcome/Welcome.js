a7a6df655fdb7913fd7f641718a3eecb4652e4c8
rushidurge2003 <rushikeshdurge7794@gmail.com>
2024-03-11 12:43:39 +0530

    updated commit

--- expensestreet/src/component/welcome/Welcome.js
+++ expensestreet/src/component/welcome/Welcome.js
@@ -2,11 +2,12 @@ import React from 'react'
 import './welcome.css'
 import Typewriter from 'typewriter-effect'
 import { useNavigate } from 'react-router-dom'
-// import { useDispatch } from 'react-redux'
-// import { useEffect } from 'react'
-// import { getUser } from '../../slice/SignUpSlice'
+import usePreventZomm from '../usePreventZoom'
+import fimg from './images/first.gif'
+import card from './images/credit-cad.png'
 
 export const Welcome = () => {
+  usePreventZomm()
 
   const navigation = useNavigate()
 
@@ -19,34 +20,44 @@ export const Welcome = () => {
 
   return (
     <>
-      <div className='welcome'>
-        <div>
-          <h1 className='text-center' style={{ fontFamily: "Workbench, sans-serif", fontSize: "100px", color: "white" }}>ExpenseStreet</h1>
+      <div className='welcome' style={{ display: "flex", justifyContent: "center" }}>
+        <div style={{ width: "60%", backgroundColor: "white", marginTop: 120 }}>
           <div>
-            <div className='d-flex justify-content-center'>
-              <div className='d-flex py-3'>
-                <span>
-                  <h1><pre style={{ fontFamily: 'Protest Revolution, sans-serif', color: "white" }}>ExpenseStreet </pre></h1>
-                </span>
-                <h1 style={{ fontFamily: 'Protest Revolution, sans-serif', color: "white" }}>
-                  <Typewriter
-                    options={{
-                      strings: ['Manage your Expenses',
-                        'Manage your Income',
-                        'Manage your Budget',
-                        'Analysis your Data'],
-                      autoStart: true,
-                      loop: true,
-                      pauseFor: 1500
-                    }}
-                  />
-                </h1>
+            <div style={{display:"flex",justifyContent:"center"}}>
+              <img src={card} alt="" draggable="false" style={{width:200}}/>
+            </div>
+            <h1 className='text-center' style={{ fontFamily: "Workbench, sans-serif", fontSize: "100px", color: "black", paddingTop: 20 }}>ExpenseStreet</h1>
+            <div>
+              <div className='d-flex justify-content-center'>
+                <div className='d-flex py-3'>
+                  <span>
+                    <h1><pre style={{ fontFamily: 'Protest Revolution, sans-serif', color: "black" }}>ExpenseStreet </pre></h1>
+                  </span>
+                  <h1 style={{ fontFamily: 'Protest Revolution, sans-serif', color: "black" }}>
+                    <Typewriter
+                      options={{
+                        strings: ['Manage your Expenses',
+                          'Manage your Income',
+                          'Manage your Budget',
+                          'Analysis your Data'],
+                        autoStart: true,
+                        loop: true,
+                        pauseFor: 1500
+                      }}
+                    />
+                  </h1>
+                </div>
               </div>
             </div>
-          </div>
-          <div className='d-flex justify-content-center'>
-            <button id='login-btn' onClick={() => { navigation("/login") }} className="btn btn-primary mx-3" style={{ width: "150px", backgroundColor: "white", color: 'black', borderColor: "white", fontWeight: "bold" }}>Login</button>
-            <button id='sub-btn' onClick={() => { navigation("/signup") }} className="btn btn-primary mx-3" style={{ width: "150px", backgroundColor: "white", color: 'black', borderColor: "white", fontWeight: "bold" }}>Sign Up</button>
+            <div className='d-flex justify-content-center'>
+              <button id='login-btn' onClick={() => { navigation("/login") }} className="btn btn-primary mx-3" style={{ width: "150px", backgroundColor: "black", color: 'white', borderColor: "white", fontWeight: "bold" }}>Login</button>
+              <button id='sub-btn' onClick={() => { navigation("/signup") }} className="btn btn-primary mx-3" style={{ width: "150px", backgroundColor: "black", color: 'white', borderColor: "white", fontWeight: "bold" }}>Sign Up</button>
+            </div>
+            <div>
+              <div>
+                <img src={fimg} alt="" draggable="false" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
