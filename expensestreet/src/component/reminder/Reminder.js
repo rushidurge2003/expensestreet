beca0195b73bbea29d537a2987180b4161ab64f0
rushidurge2003 <rushikeshdurge7794@gmail.com>
2024-03-17 18:02:59 +0530

    Reminder Updated Commit

--- expensestreet/src/component/reminder/Reminder.js
+++ expensestreet/src/component/reminder/Reminder.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import dayjs from 'dayjs'
 import {
     Form, TimePicker, Modal, Input, Select,
-    DatePicker, Card, message, Button, Col, Row
+    DatePicker, message, Button, Empty
 } from 'antd'
 import { PlusOutlined } from '@ant-design/icons';
 import { useDispatch, useSelector } from 'react-redux'
 import { addReminder, getReminderData } from '../../slice/ReminderSlice';
+import success from './images/success.png'
 
 export const Reminder = () => {
     const date = new Date()
@@ -23,14 +24,12 @@ export const Reminder = () => {
         dispatch(getReminderData(localStorage.getItem("username")))
     }
 
-
-
     const [remDesc, setRemDesc] = useState("")
     const [remTime, setRemTime] = useState("")
     const [remDate, setRemDate] = useState(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`)
     const [amount, setAmount] = useState(0)
     const [type, setType] = useState("")
-
+    const [dateRange, setDateRange] = useState(`${date.getFullYear()}/0${date.getMonth() + 1}`)
 
     const [isModalOpen, setIsModalOpen] = useState(false);
     const showModal = () => {
@@ -40,34 +39,65 @@ export const Reminder = () => {
         setIsModalOpen(false);
         const username = localStorage.getItem("username")
         dispatch(addReminder({ "username": username, "desc": remDesc, "datetime": remDate + " " + remTime, "amount": amount, "type": type }))
+        dispatch(getReminderData(localStorage.getItem("username")))
     };
     const handleCancel = () => {
         setIsModalOpen(false);
     }
 
-    // const data = state.map((x)=>{return({...x})})
+    const displayData = state?.filter((x) => {
+        if ((x.reminderDateTime).slice(0, 7).replace("-", "/") === dateRange) {
+            return ({ ...x })
+        }
+    })
+
+    console.log("Display Data : ", displayData);
+    console.log("Display Data Length : ", displayData.length);
 
     return (
         <>
             <div className='d-flex justify-content-between'>
                 <div><h5>Reminder</h5></div>
+                <DatePicker minDate={dayjs("2023/12")} defaultValue={dayjs(dateRange, "YYYY/MM")} format={"YYYY/MM"} onChange={(_, strDate) => setDateRange(strDate)} picker="month" />
                 <Button onClick={showModal} icon={<PlusOutlined />} />
             </div>
             <div>
-                <Row gutter={16}>
-                    {
-                        state.map((x,index) => {
-                            return (
-                                <Col span={4} key={index}>
-                                    <Card className='my-2' title={x.reminderDesc} bordered={true} hoverable style={{ width: 160,height:180 }}>
-                                        <p>{(x.reminderDateTime).slice(0,19).replace("T"," ")}</p>
-                                        <p>{x.type} : <strong>{x.amount}</strong></p>
-                                    </Card>
-                                </Col>
-                            )
-                        })
-                    }
-                </Row>
+                {
+                    displayData?.length > 0 ?
+                        <table className="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Date</th>
+                                    <th scope="col">Time</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Type</th>
+                                    <th scope="col">Amount</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    displayData?.map((x, index) => {
+                                        return (
+                                            <tr>
+                                                <td>{index + 1}</td>
+                                                <td>{(x.reminderDateTime).slice(0, 10)}</td>
+                                                <td>{(x.reminderDateTime).slice(11, 19)}</td>
+                                                <td>{x.reminderDesc}</td>
+                                                <td>{x.type}</td>
+                                                <td>{x.amount}</td>
+                                                <td><img src={success} width={20} /></td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table> :
+                        <Empty/>
+                }
+
             </div>
 
             <Modal title="Add Reminder" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
