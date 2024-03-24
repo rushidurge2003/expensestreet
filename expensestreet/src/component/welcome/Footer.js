d0c24e288c84c600f4d654b2368f7bdb476d923d
rushidurge2003 <rushikeshdurge7794@gmail.com>
2024-03-24 16:36:00 +0530

    lap update

--- expensestreet/src/component/welcome/Footer.js
+++ expensestreet/src/component/welcome/Footer.js
@@ -7,10 +7,7 @@ export const Footer = () => {
   return (
     <>
       <div style={{ marginTop: 30, marginBottom: 0, padding: 10, boxShadow: "0px 3px 6px 0px rgba(0,0,0,0.16),0px 3px 6px 0px rgba(0,0,0,0.23)" }}>
-        <div className='d-flex justify-content-between' style={{ paddingLeft: 40, paddingRight: 40 }}>
-          <div>
-            Get connected with us on social networks:
-          </div>
+        <div style={{ paddingLeft: 40, paddingRight: 40, display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
           <div>
             <Link className='btn btn-light btn-md mx-2 my-1' target='blank' to="https://github.com/rushidurge2003"><GithubOutlined size={10} /></Link>
             <Link className='btn btn-light btn-md mx-2 my-1' target='blank' to="https://in.linkedin.com"><LinkedinOutlined size={10} /></Link>
@@ -18,11 +15,14 @@ export const Footer = () => {
             <Link className='btn btn-light btn-md mx-2 my-1' target='blank' to="https://twitter.com/?lang=en"><TwitterOutlined size={10} /></Link>
             <Link className='btn btn-light btn-md mx-2 my-1' target='blank' to="https://www.facebook.com"><FacebookOutlined size={10} /></Link>
           </div>
+          <div>Get connected with us on social networks</div>
+          <div>
+            Contact us : <strong><a target='blank' href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=rushikeshdurge7794@gmail.com">rushikeshdurge7794@gmail.com</a></strong>
+          </div>
         </div>
         <hr />
-        <div>
-          <h6 className='text-center'>ExpenseStreet ©{new Date().getFullYear()} Created by Rushikesh Durge,Atharva Jori,Niraj Kate</h6>
-
+        <div style={{ marginBottom: 20 }}>
+          <h6 className='text-center'>ExpenseStreet ©{new Date().getFullYear()} Created by : Rushikesh Durge,Atharva Jori,Niraj Kate</h6>
         </div>
       </div>
     </>
--- expensestreet/src/component/welcome/Welcome.js
+++ expensestreet/src/component/welcome/Welcome.js
@@ -10,6 +10,7 @@ import exp1 from './images/exp1.gif'
 import exp2 from './images/exp2.png'
 import exp3 from './images/exp3.png'
 import exp4 from './images/exp4.png'
+import vid from './images/back.mp4'
 
 export const Welcome = () => {
   usePreventZomm()
@@ -26,7 +27,11 @@ export const Welcome = () => {
   return (
     <>
       <div style={{ width: "100%", height: "60vh", backgroundColor: "gray", position: "absolute", zIndex: -1 }}>
-
+        <div style={{ width: "100%", margin: '0 auto' }}>
+          <video autoPlay muted loop id="main-video" style={{ objectFit: "fill", width: "100%", height: "60vh" }}>
+            <source src={vid} />
+          </video>
+        </div>
       </div>
       {/* <div className='welcome' style={{ display: "flex", justifyContent: "center" }}> */}
       <div style={{ display: "flex", justifyContent: "center" }}>
@@ -35,15 +40,13 @@ export const Welcome = () => {
             <div style={{ display: "flex", justifyContent: "center" }}>
               <img src={applogo} alt="" draggable="false" style={{ width: 620 }} />
             </div>
-            {/* <h1 className='text-center' style={{ fontFamily: "Workbench, sans-serif", fontSize: "100px", color: "black", paddingTop: 20 }}>ExpenseStreet</h1> */}
-            {/* <h1 className='text-center' style={{ fontFamily: "Workbench, sans-serif", fontSize: "100px", color: "black", paddingTop: 20 }}>ExpenseStreet</h1> */}
             <div>
               <div className='d-flex justify-content-center'>
                 <div className='d-flex py-3'>
                   <span>
                     <h1><pre style={{ fontFamily: 'Protest Revolution, sans-serif', color: "black" }}>ExpenseStreet </pre></h1>
                   </span>
-                  <h1 style={{ fontFamily: 'Protest Revolution, sans-serif', color: "black" }}>
+                  <h1 style={{ fontFamily: 'Lora, serif', color: "black" }}>
                     <Typewriter
                       options={{
                         strings: ['Manage your Expenses',
@@ -64,17 +67,49 @@ export const Welcome = () => {
               <button id='sub-btn' onClick={() => { navigation("/signup") }} className="btn btn-primary mx-3" style={{ width: "150px", backgroundColor: "black", color: 'white', borderColor: "white", fontWeight: "bold" }}>Sign Up</button>
             </div>
             <div>
-              <div>
-                <img src={exp1} alt="" draggable="false" style={{ width: 350 }} />
+              <div className='row'>
+                <div className='col-6'>
+                  <img src={exp1} alt="" draggable="false" style={{ width: 350 }} />
+                </div>
+                <div className='col-6'>
+                  <h4 className='text-center' style={{ marginTop: 55 }}>Data Presentation</h4>
+                  <p className='px-3'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos, ex aperiam corrupti quisquam totam amet aut earum suscipit distinctio cupiditate ducimus aliquid autem facere voluptas error beatae laudantium neque voluptate sint sequi explicabo natus hic odit? Modi nobis dolores doloribus culpa magnam sequi cupiditate ex error, quibusdam, quidem non molestias.
+                  </p>
+                </div>
               </div>
-              <div>
-                <img src={exp2} alt="" draggable="false" style={{ width: 350 }} />
+              <div className='row'>
+                <div className='col-6'>
+                  <h4 className='text-center' style={{ marginTop: 55 }}>Authentication</h4>
+                  <p className='px-3'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi dolores libero necessitatibus sunt voluptatibus repellat aspernatur quas incidunt nam eveniet facilis quia quasi tempora alias reiciendis, optio voluptatem rerum similique ducimus laboriosam ea? Corrupti corporis doloremque repudiandae omnis? Blanditiis optio dolor qui obcaecati rem, dicta aut id distinctio tempora placeat?
+                  </p>
+                </div>
+                <div className='col-6'>
+                  <img src={exp2} alt="" draggable="false" style={{ width: 350 }} />
+                </div>
               </div>
-              <div>
-                <img src={exp3} alt="" draggable="false" style={{ width: 350 }} />
+              <div className='row'>
+                <div className='col-6'>
+                  <img src={exp3} alt="" draggable="false" style={{ width: 350 }} />
+                </div>
+                <div className="col-6">
+                  <h4 className="text-center" style={{ marginTop: 55 }}>Reminder</h4>
+                  <p className="px-3">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit amet, atque iure eos molestias soluta eaque necessitatibus maiores provident sapiente. Itaque totam temporibus ullam. In repellendus rerum quaerat enim qui quis amet quidem, provident ut veniam eveniet modi reiciendis est, hic ea nam commodi quo totam magni, inventore atque explicabo!
+                  </p>
+                </div>
               </div>
-              <div>
-                <img src={exp4} alt="" draggable="false" style={{ width: 350 }} />
+              <div className='row'>
+                <div className="col-6">
+                  <h4 className="text-center" style={{ marginTop: 55 }}>News</h4>
+                  <p className="px-3">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ratione vero, soluta facilis ullam perferendis porro ea magni modi id optio, quae cum, iusto dolor. Aliquid aut, fugiat ab laborum magnam soluta officiis ex possimus nobis quaerat obcaecati enim suscipit temporibus tempora distinctio! A voluptate pariatur ipsam nihil ex cumque!
+                  </p>
+                </div>
+                <div className='col-6'>
+                  <img src={exp4} alt="" draggable="false" style={{ width: 300 }} />
+                </div>
               </div>
             </div>
           </div>
