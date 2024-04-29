e6985e43473685983e51597f8638b90c9a3a18d1
rushidurge2003 <rushikeshdurge7794@gmail.com>
2024-04-30 00:49:08 +0530

    resp changes 30/4 00:16

--- src/component/welcome/WelcomeNavbar.js
+++ src/component/welcome/WelcomeNavbar.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { GithubOutlined } from '@ant-design/icons'
-import { Button } from 'antd'
+import { GithubOutlined, MenuOutlined } from '@ant-design/icons'
+import { Button, Drawer } from 'antd';
 import { useNavigate } from 'react-router-dom'
+import { useMediaQuery } from 'react-responsive'
 
 export const WelcomeNavbar = () => {
+
+  const isBreak = useMediaQuery({ query: '(max-width: 900px)' })
+
   const navigate = useNavigate()
+
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
 
   return (
     <>
@@ -13,16 +27,23 @@ export const WelcomeNavbar = () => {
         <div className="container-fluid">
           <div>
             <Link className="navbar-brand" to="/" style={{ color: "black" }}> <b>ExpenseStreet</b></Link>
-            <Button type='text' style={{ paddingTop: 8 }} onClick={() => { navigate("/about") }}><strong style={{ color: "black" }}>About</strong></Button>
-            <Link className='btn btn-light btn-sm mx-2 my-1' target='blank' to="https://github.com/rushidurge2003"><GithubOutlined size={10} /></Link>
+            <Button type='text' style={{ paddingTop: 8, display: isBreak ? "none" : "" }} onClick={() => { navigate("/about") }}><strong style={{ color: "black" }}>About</strong></Button>
+            <Link className='btn btn-light btn-sm mx-2 my-1' style={{ display: isBreak ? "none" : "" }} target='blank' to="https://github.com/rushidurge2003"><GithubOutlined size={10} /></Link>
           </div>
           <div>
-            <Link className='btn btn-dark btn-sm mx-2' to="/login">Login</Link>
-            <Link className='btn btn-dark btn-sm' to="/signup">Sign Up</Link>
+            <Link className='btn btn-dark btn-sm mx-2' style={{ display: isBreak ? "none" : "" }} to="/login">Login</Link>
+            <Link className='btn btn-dark btn-sm' style={{ display: isBreak ? "none" : "" }} to="/signup">Sign Up</Link>
+            <Button type="text" style={{ display: isBreak ? "" : "none" }} onClick={showDrawer} icon={<MenuOutlined />} />
           </div>
 
         </div>
       </nav>
+      <Drawer title="Menu" onClose={onClose} open={open} style={{display:isBreak?"":"none"}}>
+        <Button type='text' style={{ paddingTop: 8}} onClick={() => { navigate("/about") }}><strong style={{ color: "black" }}>About</strong></Button>
+        <Link className='btn btn-light btn-sm mx-2 my-1' target='blank' to="https://github.com/rushidurge2003"><GithubOutlined size={10} /></Link>
+        <Link className='btn btn-dark btn-sm mx-2' to="/login">Login</Link>
+        <Link className='btn btn-dark btn-sm' to="/signup">Sign Up</Link>
+      </Drawer>
     </>
   )
 }
